3af8c76a633594601343324e35e2a86e51ef3140
Talyshinskiy <talyshinskiy.b@gmail.com>
2024-06-23 01:01:18 +0300

    finished the webpack configuration

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,32 @@
 import Timer from "./Timer";
-import {Link, Outlet} from "react-router-dom";
+import {Link, Outlet} from 'react-router-dom'
+import logoWhite from '@/assets/logoWhite.jpg'
+import IconHome from '@/assets/iconHome.svg'
+
+
 
 export const App = () => {
+function checkTS (a: number){
 
+}
+checkTS(1)
   return (
       <div>
+        <img src={logoWhite} height={300} width={1200}/>
         <h1>Apppppp</h1>
-        <Timer/>
-        <Timer/>
+
         <Link to={'/main'}>Main</Link>
+        <IconHome fill={'red'} width={50} height={50}/>
+
+
         <br/>
         <Link to={'/about'}>About</Link>
         <br/>
         <Link to={'/quizzes'}>Quizzes</Link>
         <br/>
         <Link to={'/knowledgeDatabase'}>Knowledge Database</Link>
+        <Timer/>
+        <Timer/>
         <Outlet/>
       </div>
   )
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,13 +1,14 @@
 import path from 'path';
 import webpack from 'webpack';
 import {buildWebpack} from "./config/build/buildWebpack";
-import {BuildMode, BuildPaths} from "./config/build/types/types";
+import {BuildMode, BuildPaths, BuildPlatform} from "./config/build/types/types";
 
 
 interface EnvVariables {
-  mode: BuildMode;
-  port: number;
+  mode?: BuildMode;
+  port?: number;
   analyzer?: boolean;
+  platform?: BuildPlatform
 }
 
 export default (env: EnvVariables) => {
@@ -15,6 +16,7 @@ export default (env: EnvVariables) => {
     output: path.resolve(__dirname, 'build'),
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
     html: path.resolve(__dirname, 'public', 'index.html'),
+    public: path.resolve(__dirname, 'public'),
     src: path.resolve(__dirname, 'src')
   }
 
@@ -22,7 +24,8 @@ export default (env: EnvVariables) => {
     port: env.port ?? 3000,
     mode: env.mode ?? 'development',
     paths,
-    analyzer: env.analyzer
+    analyzer: env.analyzer,
+    platform: env.platform ?? "desktop"
   })
   return config
 }
